6b6b955e3a925b8ea58f99913689195b3634e8d2
SaraHemidova <aytanhuseynli24>
2025-04-30 10:18:03 +0400

    added task

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fronttoback33.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
